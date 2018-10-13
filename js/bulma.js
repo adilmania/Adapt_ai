054820c75bf34173242426397a7a7cd3226aa063
Adil Mania <adil.mania.pro@gmail.com>
2018-10-13 21:12:12 +0200

    :tada: Init Commit

---
+++ js/bulma.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    document.querySelectorAll('[data-target]').forEach((result) => {
+        result.onclick = () => {
+            let modal = document.getElementById(result.getAttribute('data-target'))
+            modal.classList.toggle("is-active")
+            modal.querySelector('.delete').onclick = () => { modal.classList.toggle("is-active") }
+        }
+    })
+}
